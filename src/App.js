b87959bf698e6733495a2bbbfff5ad5be4bfe1d4
renanberton <renan_berton@yahoo.com.br>
2024-02-02 12:46:18 -0300

    Feature: Exclusão de arquivos não usados + Estilização do projeto + Responsividade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form, Button, Alert } from 'react-bootstrap';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortenedUrl, setShortenedUrl] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const shortenUrl = async () => {
+    try {
+      if (!originalUrl) {
+        setErrorMessage('Por favor, digite uma URL válida.');
+        setShortenedUrl('');
+      } else {
+        const response = await axios.post('http://localhost:5000/shorten', {
+          originalUrl,
+        });
+
+        setShortenedUrl(response.data.shortUrl);
+        setErrorMessage('');
+      }
+    } catch (error) {
+      console.error('Error shortening URL:', error);
+      setErrorMessage('Ocorreu um erro ao encurtar a URL. Tente novamente.');
+      setShortenedUrl('');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Encurtador de Link</h1>
+      </div>
+      <Form className='form'>
+        <Form.Group controlId="formOriginalUrl" className='form-url-original'>
+          <Form.Label className='text-url'>Insira o Link Original</Form.Label>
+          <Form.Control
+            className='input'
+            type="url"
+            placeholder="Digite o Link original"
+            value={originalUrl}
+            onChange={(e) => setOriginalUrl(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" className='btn-url-original' onClick={shortenUrl}>
+          Encurtar Link
+        </Button>
+      </Form>
+
+      {errorMessage && <Alert variant="danger" className="mt-3 new-url">{errorMessage}</Alert>}
+
+      {shortenedUrl && !errorMessage && (
+        <div className="mt-3 new-url">
+          <p>Seu Novo Link</p>
+          <a href={shortenedUrl} target="_blank" rel="noopener noreferrer">
+            {shortenedUrl}
+          </a>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
