6e42f34fc236183e795f9839e1ad038ed94cfc1b
renanberton <renan_berton@yahoo.com.br>
2024-02-02 19:02:53 -0300

    agora foi

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ const App = () => {
         setErrorMessage('Por favor, digite uma URL válida.');
         setShortenedUrl('');
       } else {
-        console.log('Backend URL:', process.env.REACT_APP_BACKEND_URL);
-        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/shorten`, {
+        const response = await axios.post(`https://url-shortner-brasil-back-end.netlify.app/shorten`, {
           originalUrl,
         });
         
